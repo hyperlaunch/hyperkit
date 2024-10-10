05baec32b8a5bf1eb27eeb2894255f254350c562
Chris Garrett <chris@c8va.com>
2024-10-10 14:31:17 +0100

    Revert "Quick transition refactor test"
    This reverts commit fd263fe838f14806910ff65292247e2c5afcc5aa.

--- packages/elements/src/transition.ts
+++ packages/elements/src/transition.ts
@@ -1,73 +1,86 @@
 export class HyperkitTransition extends HTMLElement {
-	private isExiting = false;
+	private enterClass = this.getAttribute("enter-class") ?? "";
+	private enterFromClass = this.getAttribute("enter-from-class") ?? "";
+	private enterToClass = this.getAttribute("enter-to-class") ?? "";
+	private exitClass = this.getAttribute("exit-class") ?? "";
+	private exitFromClass = this.getAttribute("exit-from-class") ?? "";
+	private exitToClass = this.getAttribute("exit-to-class") ?? "";
 
 	connectedCallback() {
-		// Automatically start the enter transition if the 'enter-on-connect' attribute is present
-		if (this.hasAttribute("enter-on-connect")) {
-			this.enter();
-		}
+		if (this.hasAttribute("enter-on-connect")) this.enter();
 	}
 
 	enter() {
-		// Set the initial state to 'entering'
-		this.setDataState("entering");
+		this.removeClasses(this.exitClass);
+		this.removeClasses(this.exitFromClass);
+		this.removeClasses(this.exitToClass);
 
-		// Ensure the transition starts in the next animation frame
+		this.removeAttribute("hidden");
+
+		this.applyClasses(this.enterClass, this.enterFromClass);
+
 		requestAnimationFrame(() => {
-			requestAnimationFrame(() => {
-				this.setDataState("entered");
-			});
+			this.removeClass(this.enterFromClass);
+			this.applyClass(this.enterToClass);
 		});
 
-		// Listen for the transition to complete before dispatching the 'entered' event
-		this.addEventListener(
-			"transitionend",
-			(event) => {
-				if (event.target === this && !this.isExiting) {
-					this.dispatchEvent(
-						new CustomEvent("change", { detail: { state: "entered" } }),
-					);
-				}
-			},
-			{ once: true },
+		setTimeout(
+			() =>
+				this.dispatchEvent(
+					new CustomEvent("change", { detail: { state: "entered" } }),
+				),
+			this.getTransitionDuration(),
 		);
 	}
 
 	exit() {
-		// Set the flag to mark that we are exiting
-		this.isExiting = true;
+		this.removeClasses(this.enterClass);
+		this.removeClasses(this.enterFromClass);
+		this.removeClasses(this.enterToClass);
 
-		// Set the initial state to 'exiting'
-		this.setDataState("exiting");
+		this.applyClasses(this.exitClass, this.exitFromClass);
 
-		// Ensure the transition starts in the next animation frame
 		requestAnimationFrame(() => {
-			requestAnimationFrame(() => {
-				this.setDataState("exited");
-			});
-		});
+			this.removeClass(this.exitFromClass);
+			this.applyClass(this.exitToClass);
 
-		// Listen for the transition to complete before dispatching the 'exited' event and hiding the element
-		this.addEventListener(
-			"transitionend",
-			(event) => {
-				if (event.target === this) {
-					this.setAttribute("hidden", "");
+			setTimeout(
+				() =>
 					this.dispatchEvent(
 						new CustomEvent("change", { detail: { state: "exited" } }),
-					);
-					this.isExiting = false;
-				}
-			},
-			{ once: true },
-		);
+					),
+				this.getTransitionDuration(),
+			);
+		});
 	}
 
-	private setDataState(state: "entering" | "entered" | "exiting" | "exited") {
-		this.setAttribute("data-state", state);
+	private addClasses(classNames: string) {
+		this.classList.add(...classNames.split(" ").filter(Boolean));
+	}
+
+	private removeClasses(classNames: string) {
+		this.classList.remove(...classNames.split(" ").filter(Boolean));
+	}
+
+	private applyClasses(...classList: string[]) {
+		for (const cls of classList) this.addClasses(cls);
+	}
+
+	private applyClass(className: string) {
+		this.addClasses(className);
+	}
+
+	private removeClass(className: string) {
+		this.removeClasses(className);
+	}
+
+	private getTransitionDuration() {
+		const duration = window
+			.getComputedStyle(this)
+			.getPropertyValue("transition-duration");
+		return Number.parseFloat(duration) * 1000;
 	}
 }
 
-if (!customElements.get("hyperkit-transition")) {
+if (!customElements.get("hyperkit-transition"))
 	customElements.define("hyperkit-transition", HyperkitTransition);
-}
