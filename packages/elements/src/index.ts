07cfc2f8334521e2f9cd706fcb5189e904263917
Chris Garrett <chris@c8va.com>
2024-10-19 14:56:27 +0100

    Moar tweaks

--- packages/elements/src/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./arrow-nav";
-export * from "./calendar";
-export * from "./detail";
-export * from "./masked-input";
-export * from "./modal";
-export * from "./popover";
-export * from "./transition";
